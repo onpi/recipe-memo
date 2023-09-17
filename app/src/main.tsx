51e4178c527a5ae65b7633466e86179afdb8b488
小野遼太 <o.ryota1001@gmail.com>
2023-09-17 12:42:38 +0900

    PWAの設定追加

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(function () {
+    console.log('Service Worker Registered');
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
